8a175f0fd8ca9dbadfa146e3869bf87e22617908
Jake Russ <jake.russ@tntp.org>
2016-06-15 15:00:51 -0400

    Remove export

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,4 @@
 #' @importFrom ggplot2 theme_minimal
-ggplot2::theme_minimal
 
 # Create TNTP color palette
 # colors: [1]dark blue, [2]med blue, [3]light blue, [4]green, [5]orange, [6]gold, [7]dark grey, [8]med grey, [9]light grey
