159aa3b888b1903bfbea7b87645ecbb7f53861df
Jake Russ <jake.russ@tntp.org>
2016-06-15 16:02:09 -0400

    Add package level docs

---
+++ R/Rtntp.R
@@ -0,0 +1,8 @@
+#' @title Data Analysis Tools Customized for TNTP
+#' @name Rtntp
+#' @description An internal TNTP R package that includes functions and templates
+#' customized with TNTP branding. It is intended for this R package to be a
+#' one-stop shop for R tools by analysts and other data users at TNTP.
+#' @docType package
+#' @aliases Rtntp Rtntp-package
+NULL
