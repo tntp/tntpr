6e808b8106f191644912c53605347bb1c6579e0b
Jake Russ <jake.russ@tntp.org>
2016-06-15 14:17:34 -0400

    Add utils.r
    Import theme_minimal; move tntp.palette out of theme_tntp.R

---
+++ R/utils.R
@@ -0,0 +1,7 @@
+#' @importFrom ggplot2 theme_minimal
+#' @export
+ggplot2::theme_minimal
+
+# Create TNTP color palette
+# colors: [1]dark blue, [2]med blue, [3]light blue, [4]green, [5]orange, [6]gold, [7]dark grey, [8]med grey, [9]light grey
+tntp.palette <- c("#034772","#2888BC","#73B7CE", "#699D46", "#EA8936", "#F9C347","#58595B", "#7D7E81", "#C1C2C4")
