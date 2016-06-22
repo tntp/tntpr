f48fb667f5129a6f1b3cd2b51eee055701ccf76f
Jake Russ <jake.russ@tntp.org>
2016-06-22 15:49:14 -0400

    Rename package Rtntp -> tntpr

--- R/tntpr.R
+++ R/tntpr.R
@@ -1,9 +1,9 @@
 #' @title Data Analysis Tools Customized for TNTP
-#' @name Rtntp
+#' @name tntpr
 #' @description An internal TNTP R package that includes functions and templates
 #' customized with TNTP branding. It is intended for this R package to be a
 #' one-stop shop for R tools by analysts and other data users at TNTP.
 #' @docType package
-#' @aliases Rtntp Rtntp-package
+#' @aliases tntpr tntpr-package
 #' @importFrom ggplot2 theme_minimal
 NULL
