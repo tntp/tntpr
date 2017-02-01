6a8be471d3981c8b0c7f3e90a25be95a01dbcc8d
Sam Firke <samuel.firke@gmail.com>
2017-02-01 16:38:50 -0500

    early stab at bar_chart_means

--- R/bar_chart_means.R
+++ R/bar_chart_means.R
@@ -1,12 +1,11 @@
-#' Bar chart of counts with TNTP polish
+#' Bar chart of means with TNTP polish
 #'
-#' Takes a user supplied data frame and turns the designated column(s) into
-#' an N bar chart (uses position dodge from ggplot2).
-#'@rdname bar_chart_counts
+#' Takes a user supplied data frame and turns the designated columns into
+#' a bar chart of means.
+#'@rdname bar_chart_means
 #'@param df the data.frame to be used in the bar chart
-#'@param var unquoted column name for variable to count
-#'@param group_var (optional) unquoted column name for group variable.  If this is specified, you get a 2-variable clustered bar chart.  If left blank, a single variable bar chart.
-#'@param labels should labels show the count (\code{"n"}) or the percentage (\code{"pct"})?
+#'@param var unquoted column name for variable to take mean of
+#'@param group_var (optional) unquoted column name for group variable.  If this is specified, you get the means broken out by subgroup.  If left blank, a single bar showing an overall mean.
 #'@param var_color color for non-grouped charts; set to medium_blue by default. For both this and \code{group_colors}, strings will be tried in \code{palette_tntp} automatically.  So \code{c("orange", "dark_blue")} will get you the official TNTP colors, while \code{c("orange", "blue")} will get you TNTP orange but generic blue.
 #'@param group_colors character vector of group colors, if a specific palette is desired
 #'@param title main chart title
@@ -37,42 +36,42 @@
 #'# Specify color by group
 #'  bar_chart_counts(mtcars, am, cyl, group_colors = c("orange", "green", "dark_blue"), labels = "pct")
 bar_chart_counts <- function(df,
-                           var,
-                           group_var,
-                           labels        = "n",
-                           var_color    = "medium_blue",
-                           group_colors,
-                           title        = NULL,
-                           var_label,
-                           digits       = 1,
-                           font         = "Segoe UI",
-                           font_size    = 12
-                           ){
-
+                             var,
+                             group_var,
+                             labels        = "n",
+                             var_color    = "medium_blue",
+                             group_colors,
+                             title        = NULL,
+                             var_label,
+                             digits       = 1,
+                             font         = "Segoe UI",
+                             font_size    = 12
+){
+  
   # QC: Throw an error if object supplied to df is not a data.frame -----------
   testthat::expect(exp = is.data.frame(df),
                    "You must supply a data.frame to the df argument")
-
+  
   # QC: Throw an error if var was not specified  ------------------------------
   testthat::expect(exp = !missing(var),
                    "You must supply a column name to the var argument")
-
-
+  
+  
   # Create a plot_data object -------------------------------------------------
   # plot_data should contain user specified column and its factor equivalent
-
+  
   # Check if a grouping variable was specified
   if(missing(group_var)){
-
+    
     plot_data <- df %>%
       dplyr::select_(.dots = list(vec = lazyeval::lazy(var))) %>%
       dplyr::mutate(vec.factor = as.factor(vec)) %>%
       dplyr::group_by(vec.factor) %>%
       dplyr::tally() %>%
       dplyr::mutate(perc = n / sum(n))
-
+    
   } else {
-
+    
     plot_data <- df %>%
       dplyr::select_(.dots = list(vec       = lazyeval::lazy(var),
                                   group.vec = lazyeval::lazy(group_var))) %>%
@@ -84,60 +83,60 @@ bar_chart_counts <- function(df,
       dplyr::mutate(perc = n / sum(n)) %>%
       dplyr::ungroup() %>%
       tidyr::complete(vec.factor, group.factor, fill = list(n = NA, perc = NA))
-
+    
   }
-
+  
   # Create a color palette ----------------------------------------------------
-
+  
   # Check if group_var is supplied
   if(!missing(group_var)){
-
+    
     if(missing(group_colors)) {
-
+      
       # QC: If group_var is supplied, but no colors, create a color palette
       #     while also making sure there are enough colors for each group
-
+      
       # Select a color for each level of the factor-ed grouping variable. Must
       # be less than or equal to 11 (because we only have 11 TNTP colors)
       num_groups <- plot_data$group.factor %>% levels() %>% length()
-
+      
       testthat::expect_lte(num_groups, 11,
                            "The maximum number of levels allowed in group_var is 11")
-
+      
       tntp_col_pal <- palette_tntp("dark_blue", "medium_blue", "light_blue",
                                    "orange", "gold", "green", "dark_grey",
                                    "medium_grey", "light_grey", "white",
                                    "black")[1:num_groups]
-
+      
     } else {
-
+      
       # QC: Throw an error if the number of levels in supplied group_var does
       # not equal the number of group_colors
       num_group_var <- plot_data$group.factor %>% levels() %>% length()
       num_group_col <- group_colors %>% length()
-
+      
       testthat::expect_identical(num_group_var, num_group_col,
                                  info = "The number of group_colors must equal
                                  the number of levels supplied to group_var")
       # Switch color name strings to the HEX codes
       tntp_col_pal <- swap_colors(group_colors)
-
+      
     }
   }
-
+  
   # Check whether user specified an x axis label ------------------------------
   if(missing(var_label)) {
-
+    
     var_label <- deparse(substitute(var))
-
+    
   }
-
+  
   # Build the N bar chart -----------------------------------------------------
   # Condition on presence of group_var
-
-    if(missing(group_var)){
+  
+  if(missing(group_var)){
     
-
+    
     if(labels == "pct"){
       
       nbc <- ggplot(data = plot_data, aes(x = vec.factor, y = perc)) +
@@ -185,13 +184,13 @@ bar_chart_counts <- function(df,
                              size = font_size * 0.35)
     }
   }
-
+  
   # Polish the plot to presentation standards ---------------------------------
-
+  
   # so labels don't get cropped, set the y scale 5% higher than the highest bar
   max_height <- dplyr::if_else(labels == "pct",
-                        max(plot_data$perc, na.rm = TRUE) * 1.1,
-                        max(plot_data$n, na.rm = TRUE) * 1.1)
+                               max(plot_data$perc, na.rm = TRUE) * 1.1,
+                               max(plot_data$n, na.rm = TRUE) * 1.1)
   nbc <- nbc +
     scale_y_continuous(expand = c(0, 0), # do people mind there being no whitespace at the bottom?  There's none in Excel
                        limits = c(0, max_height)) +
@@ -206,41 +205,19 @@ bar_chart_counts <- function(df,
           axis.title.x     = element_text(family = font,
                                           size   = font_size),
           axis.title.y     = element_blank(),
-
+          
           legend.key       = element_blank(),
           legend.position  = "bottom",
           legend.text      = element_text(family = font,
                                           size   = font_size),
           legend.title     = element_blank(),
-
+          
           panel.background = element_blank(),
           panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(),
-
+          
           plot.title       = element_text(family = font,
                                           face   = "bold",
                                           size   = font_size))
-    nbc
-}
-
-# function to swap in custom TNTP colors
-
-swap_colors <- function(x){
-  plyr::mapvalues(x,
-                  from         = c("dark_blue",   "medium_blue",
-                                   "light_blue",  "green",
-                                   "orange",      "gold",
-                                   "dark_grey",   "dark_gray",
-                                   "medium_grey", "medium_gray",
-                                   "light_grey",  "light_gray",
-                                   "white",       "black"),
-
-                  to           = c("#034772", "#2888BC",
-                                   "#73B7CE", "#699D46",
-                                   "#EA8936", "#F9C347",
-                                   "#58595B", "#58595B",
-                                   "#7D7E81", "#7D7E81",
-                                   "#C1C2C4", "#C1C2C4",
-                                   "#FFFFFF", "#000000"),
-                  warn_missing = FALSE)
+  nbc
 }
