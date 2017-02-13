f77623eea23f80041928d8fbe4558b8272d38133
Jake Russ <jake.russ@tntp.org>
2017-02-13 11:15:23 -0500

    Add support for title, subtitle, and caption text options; switch position-text checking to use match.args

--- R/theme_tntp.R
+++ R/theme_tntp.R
@@ -10,7 +10,12 @@
 #' @param base_size base font size
 #' @param base_family base font family
 #' @param grid_color color for major gridlines
-#' @param title_align Alignment of main title, defaults to "center;" also accepts "left" or "right"
+#' @param title_align alignment of main title, defaults to "center"; also accepts "left" or "right"
+#' @param title_color color of title text
+#' @param subtitle_align alignment of sub-title, defaults to "center"; also accepts "left" or "right"
+#' @param subtitle_color color of subtitle text
+#' @param caption_align alignment of caption, defaults to "right"; also accepts "left" or "center"
+#' @param caption_color color of caption text
 #' @export
 #' @rdname theme_tntp
 #' @examples
@@ -27,40 +32,74 @@
 #' p + theme_tntp(grid_color = "white")
 #' # Use left alignment for title
 #' p + theme_tntp(title_align = "left")
+#' # Text positions and colors
+#' ggplot(mtcars, aes(mpg, wt)) +
+#' geom_point() +
+#'  labs(x        = "Fuel effiiency (mpg)",
+#'       y        = "Weight (tons)",
+#'       title    = "Main ggplot2 title",
+#'       subtitle = "Subtitle for demo plot",
+#'       caption  = "Brought to you by tntpr") +
+#'  theme_tntp(base_size      = 12,
+#'             base_family    = "Segoe UI",
+#'             title_align    = "left",
+#'             title_color    = "red",
+#'             title_size     = 12,
+#'             subtitle_align = "center",
+#'             subtitle_color = "blue",
+#'             subtitle_size  = 10,
+#'             caption_align  = "right",
+#'             caption_color  = "darkolivegreen4",
+#'             caption_size   = 8,
+#'             grid_color     = "gold")
 
 theme_tntp <- function(show_legend_title = TRUE,
                        base_size         = 12,
                        base_family       = "Segoe UI",
                        grid_color        = "grey93",
-                       title_align       = "center") {
+                       title_align       = "center",
+                       title_color       = "black",
+                       title_size        = 12,
+                       subtitle_align    = "center",
+                       subtitle_color    = "black",
+                       subtitle_size     = 12,
+                       caption_align     = "right",
+                       caption_color     = "black",
+                       caption_size      = 12) {
 
   # As of v2.2 ggplot2 left aligns titles by default. -------------------------
-  # title_align allows tntpr users to quickly change
+  # title_align and subtitle_align allows tntpr users to quickly change
 
-  # Accepted alignment positions for plot title
-  positions <- c("center", "left", "right")
-  alignment <- tolower(title_align)
+  # Check alignment positions for plot title and subtitle
+  title_align    <- match.arg(title_align,    c("center", "left", "right"))
+  subtitle_align <- match.arg(subtitle_align, c("center", "left", "right"))
+  caption_align  <- match.arg(caption_align,  c("center", "left", "right"))
 
-  # Throw an error if supplied alignment doesn't exist
-  if(!(alignment %in% positions)) {
-    stop("Supplied title alignment is not available. Accepted options are \"center\", \"left\", or \"right\". Please check spelling.")
-  }
-
-  h_just_num <- switch(alignment,
-                         center = 0.5,
-                         left   = 0,
-                         right  = 1)
+  # Convert text position to a numeric value to supply
+  title_h_just    <- switch(title_align,    left = 0, center = 0.5, right = 1)
+  subtitle_h_just <- switch(subtitle_align, left = 0, center = 0.5, right = 1)
+  caption_h_just  <- switch(caption_align,  left = 0, center = 0.5, right = 1)
 
   # Build the theme -----------------------------------------------------------
   # Starts with theme_minimal and then modifies some parts
-  result <- theme_minimal(base_size  = base_size, base_family = base_family) %+replace%
+  result <- theme_minimal(base_size   = base_size,
+                          base_family = base_family) %+replace%
     theme(legend.position  = "bottom",        # Move legend to bottom
           legend.key       = element_blank(), # Remove border from legend boxes
           panel.grid.major = element_line(color = grid_color),
           panel.grid.minor = element_blank(), # Remove minor gridlines entirely
           axis.line.y      = element_line(color = "black", size = 0.25),
           axis.line.x      = element_line(color = "black", size = 0.25),
-          plot.title       = element_text(hjust = h_just_num)
+          plot.title       = element_text(hjust  = title_h_just,
+                                          colour = title_color,
+                                          size   = title_size),
+          plot.subtitle    = element_text(hjust  = subtitle_h_just,
+                                          colour = subtitle_color,
+                                          size   = subtitle_size,
+                                          margin = margin(7)),
+          plot.caption     = element_text(hjust  = caption_h_just,
+                                          colour = caption_color,
+                                          size   = caption_size)
     )
   if(!show_legend_title){
     result <- result + theme(legend.title = element_blank())
