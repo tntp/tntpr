226a61323d1a8b6b62d4967688a2e1de89c67ed5
Sam Firke <samuel.firke@gmail.com>
2018-03-29 15:37:46 -0400

    export factorize_agreement

--- R/factorize_agreement.R
+++ R/factorize_agreement.R
@@ -1,6 +1,20 @@
 # Turns agreement vector into an ordered factor
 # You can supply your own set of strings and it will use those levels
 
+#' Title
+#'
+#' @param x the vector to factorize
+#' @param lvls (optional) the levels, in order, of the resulting factor
+#'
+#' @return the vector, now as an ordered factor.  Currently the export will always return lowercase.
+#' @export
+#'
+#' @examples
+#'
+#' x <- c("Strongly Agree", "Disagree")
+#' janitor::tabyl(x) # not ordered correctly
+#' y <- factorize_agreement(x)
+#' janitor::tabyl(y) # ordered correctly, shows missing levels
 factorize_agreement <- function(x, lvls){
   x <- tolower(x)
   if(missing(lvls)){ lvls <- c("strongly agree", "agree", "somewhat agree", "somewhat disagree", "disagree", "strongly disagree") }
