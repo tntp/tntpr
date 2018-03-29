53ac0223fb519ae5565ab56d3f027c6387cd6c55
Sam Firke <samuel.firke@gmail.com>
2018-03-29 15:19:10 -0400

    add draft of factorize_agreement function

--- R/factorize_agreement.R
+++ R/factorize_agreement.R
@@ -1,14 +1,17 @@
 # Turns agreement vector into an ordered factor
 # You can supply your own set of strings and it will use those levels
 
-# factorize_agreement_vec <- function(){
-
-# }
+factorize_agreement <- function(x, lvls){
+  x <- tolower(x)
+  if(missing(lvls)){ lvls <- c("strongly agree", "agree", "somewhat agree", "somewhat disagree", "disagree", "strongly disagree") }
+  x <- factor(x, levels = lvls)
+  x
+}
 
 # Searches a data.frame for vectors where the six common agreement levels make up >50% of values
 # Can supply own set of strings; calls factorize_agreement_vec
 # Throw warning with values that don't fit the paradigm
 # Returns data.frame with factorization completed in place
-# factorize_agreement_df <- function(){
+#  factorize_agreement_df <- function(x, lvls){
 
 # }
