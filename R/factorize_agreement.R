9718a56e525695b0c547f1485277795371345173
Sam Firke <samuel.firke@gmail.com>
2018-03-29 15:02:03 -0400

    clean up error addition of join R files, make stub for factorize agreemt

---
+++ R/factorize_agreement.R
@@ -0,0 +1,14 @@
+# Turns agreement vector into an ordered factor
+# You can supply your own set of strings and it will use those levels
+
+# factorize_agreement_vec <- function(){
+
+# }
+
+# Searches a data.frame for vectors where the six common agreement levels make up >50% of values
+# Can supply own set of strings; calls factorize_agreement_vec
+# Throw warning with values that don't fit the paradigm
+# Returns data.frame with factorization completed in place
+# factorize_agreement_df <- function(){
+
+# }
