b7599a16164b0b8809d7320e6bd7435f8ad8e958
Sam Firke <samuel.firke@gmail.com>
2018-04-13 09:00:14 -0400

    document & export add_cumulative

--- R/add_cumulative.R
+++ R/add_cumulative.R
@@ -1,42 +1,55 @@
-add_cumulative <- function(df, colname, dir = "down"){
+#' @title Add a cumulative sum column to a data.frame.
+#'
+#' @description A tidyverse-style function to add a cumsum column.
+#'
+#' @param dat data.frame to add cumulative sum to
+#' @param colname specify the unquoted name of the column to sum, or leave it blank in which case the function will default first to a column called "valid_percent" and then for "percent".  These defaults support running this function on the result of calls to \code{janitior::tabyl}.  If no colname is supplied and these default columns are absent in the data.frame, the function will error.
+#' @param dir direction to sum; defaults to "down" but can sum from the bottom of a data.frame with "up".  In this case the resulting column name will be "cumulative_up".
+#'
+#' @return a data.frame.
+#' @export
+#'
+#' @examples
+#' library(janitor)
+#' mtcars %>%
+#'   add_cumulative(mpg)
+#'
+#' mtcars %>%
+#'   tabyl(cyl) %>%
+#'   add_cumulative()
+#'
+#' # Vector with an NA
+#' x <- c(0, 1, 2, 3, 3, 3, NA)
+#'
+#' x %>%
+#'   tabyl() %>%
+#'   add_cumulative()
+#'
+#' x %>%
+#'   tabyl() %>%
+#'   add_cumulative(dir = "up")
+
+add_cumulative <- function(dat, colname, dir = "down"){
 
   if(!missing(colname)){
     colname <- rlang::enquo(colname)
-  } else if("valid_percent" %in% names(df)) {
+  } else if("valid_percent" %in% names(dat)) {
   colname <- rlang::sym("valid_percent")
-  } else if("percent" %in% names(df)){
+  } else if("percent" %in% names(dat)){
     colname <- rlang::sym("percent")
   } else {
-    stop("\"colname\" not specified and default columns valid_percent and percent are not present in data.frame df")
+    stop("\"colname\" not specified and default columns valid_percent and percent are not present in data.frame dat")
   }
 
-  target <- dplyr::pull(df, !! colname)
+  target <- dplyr::pull(dat, !! colname)
 
   if(dir == "up"){
     target <- rev(target)
   }
-  df$cumulative <- cumsum(ifelse(is.na(target), 0, target)) + target*0 # an na.rm version of cumsum, from https://stackoverflow.com/a/25576972
+  dat$cumulative <- cumsum(ifelse(is.na(target), 0, target)) + target*0 # an na.rm version of cumsum, from https://stackoverflow.com/a/25576972
   if(dir == "up"){
-    df$cumulative <- rev(df$cumulative)
+    dat$cumulative <- rev(dat$cumulative)
+    names(dat)[names(dat) %in% "cumulative"] <- "cumulative_up"
   }
-  df
+  dat
 }
-
-# Examples
-
-library(dplyr)
-mtcars %>%
-  add_cumulative(mpg)
-
-library(janitor)
-mtcars %>%
-  tabyl(cyl) %>%
-  add_cumulative()
-
-c(0, 1, 2, 3, 3, 3, NA) %>%
-  tabyl() %>%
-  add_cumulative()
-
-c(0, 1, 2, 3, 3, 3, NA) %>%
-  tabyl() %>%
-  add_cumulative(dir = "up")
