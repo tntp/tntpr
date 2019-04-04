b8fce537239bc8b570fc10e1ea7c1a5d2f1e7ad8
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2019-04-04 15:14:23 -0600

    Add the header_tntp function

---
+++ R/header_tntp.R
@@ -0,0 +1,22 @@
+#' Insert header_script_tntp.
+#'
+#' Call this function to insert the header from 'TNTP R Shell.R'.
+#'
+#' @export
+header_tntp <- function() {
+  rstudioapi::insertText("# TITLE: [enter]
+# AUTHOR(S): [enter]
+
+# DESCRIPTION: [enter a few comments about what this script does]
+
+# Load packages - first pacman, installing if necessary, then others
+if (!require(\"pacman\")) install.packages(\"pacman\"); library(pacman)
+p_load(tidyverse, janitor, readxl) # add more here as needed
+if (!suppressPackageStartupMessages(require(\"tntpr\"))) {p_load(devtools) devtools::install_github(\"tntpr/tntpr\", force = TRUE)}
+
+# Load data
+
+# Cleaning
+
+# Analysis")
+}
