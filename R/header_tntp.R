e6e320cc088c875d0ac29928d84ae7b7342b15f2
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2019-04-29 12:20:45 -0600

    TNTP R Script Header; updated loading packages to be more explicit

--- R/header_tntp.R
+++ R/header_tntp.R
@@ -11,8 +11,8 @@ header_tntp <- function() {
 
 # Load packages - first pacman, installing if necessary, then others
 if (!require(\"pacman\")) install.packages(\"pacman\"); library(pacman)
-p_load(here, readxl, tidyverse, janitor) # add more here as needed
-if (!suppressPackageStartupMessages(require(\"tntpr\"))) {p_load(devtools); devtools::install_github(\"tntpr/tntpr\")}
+pacman::p_load(here, readxl, tidyverse, janitor) # add more here as needed
+if (!suppressPackageStartupMessages(require(\"tntpr\"))) {pacman::p_load(devtools); devtools::install_github(\"tntpr/tntpr\")}; pacman::p_load(tntpr)
 
 # Load data
 
