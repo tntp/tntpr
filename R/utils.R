750e75184fbd24759972eb62c0bac68f9938f94d
Sam Firke <sam.firke@tntp.org>
2020-06-03 20:56:44 -0400

    make Dustin the maintainer, instruct that font_import() comes from the extrafont package

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 .onAttach <- function(libname, pkgname) {
 
 import <- "If this is your first time using tntpr on this computer, or you have recently
-updated R, please import fonts by running the command font_import(). Doing so
+updated R, please import fonts by running the command font_import() from the extrafont package. Doing so
 ensures you have the Segoe UI font loaded."
 
 packageStartupMessage(import, collapse = "\n")
