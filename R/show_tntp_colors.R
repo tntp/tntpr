8d61cf47ee7503bc60715fa10d77e249c7283b39
Dustin Pashouwer <dustin.pashouwer@tntp.org>
2020-07-08 14:49:00 -0600

    added documentation to show_tntp_colors function

--- R/show_tntp_colors.R
+++ R/show_tntp_colors.R
@@ -1,9 +1,14 @@
 #' Show TNTP color palette
 #'
+#'A quick and dirty way to show tntp colors in a plot.
+#'
+#' @param palette Default is 'all' tntp colors, but can also select a specific
+#' palette by passing either 'likert' or 'likert_orange_to_green
+#'
 #' @return
 #' @export
 #'
-#' @examples
+#' @examples show_tntp_colors(palette = "likert")
 show_tntp_colors <- function(palette = "all"){
   if(palette == "likert"){
     scales::show_col(tntpr:::colors_tntp_likert)
