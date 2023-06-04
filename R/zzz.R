e221c01971a05717245cc1e9fc39c0f3971ff0ff
Dustin Pashouwer <dpashouwer@users.noreply.github.com>
2023-06-04 13:43:25 -0600

    import "Segoe" fonts from the 'inst/fonts' folder if they don't exist

--- R/zzz.R
+++ R/zzz.R
@@ -9,23 +9,22 @@
   # adapted from hrbrthemes
 
   if (.Platform$OS.type == "windows")  { # nocov start
-    windowsFonts <- grDevices::windowsFonts
+    windowsFonts <- grDevices::windowsFonts()
     if (interactive()) packageStartupMessage("Registering Windows fonts with R")
     extrafont::loadfonts("win", quiet = TRUE)
   }
 
   if (getOption("tntpr.loadfonts", default = FALSE)) {
     if (interactive()) packageStartupMessage("Registering PDF & PostScript fonts with R")
-    pdfFonts <- grDevices::pdfFonts
-    postscriptFonts <- grDevices::postscriptFonts
+    pdfFonts <- grDevices::pdfFonts()
+    postscriptFonts <- grDevices::postscriptFonts()
     extrafont::loadfonts("pdf", quiet = TRUE)
     extrafont::loadfonts("postscript", quiet = TRUE)
   }
 
   fnt <- extrafont::fonttable()
   if (!any(grepl("Segoe", fnt$FamilyName))) {
-    packageStartupMessage("NOTE: Segoe fonts are required to use this theme.")
-    packageStartupMessage("      Please use tntpr::import_fonts() to install Segoe UI")
+    extrafont::ttf_import(file.path(system.file("fonts", package = 'tntpr')))
   } # nocov end
 
 }
