dd0159bb2e2bfa9b31ddb9836a3124b278450a81
Sam Talcott <sam.talcott@tntp.org>
2023-07-18 15:07:53 -0400

    Multi-line comments / test commit

--- R/tntp_style.R
+++ R/tntp_style.R
@@ -15,15 +15,20 @@ tntp_style <- function() {
                             size=28,
                             face="bold",
                             color="#222222"),
-  #This sets the font, size, type and colour of text for the chart's subtitle, as well as setting a margin between the title and the subtitle
+  #This sets the font, size, type and colour of text for the chart's subtitle,
+  #as well as setting a margin between the title and the subtitle
   plot.subtitle = ggplot2::element_text(family=font,
                                size=22,
                                margin=ggplot2::margin(9,0,9,0)),
   plot.caption = ggplot2::element_blank(),
-  #This leaves the caption text element empty, because it is set elsewhere in the finalise plot function
+  #This leaves the caption text element empty, because it is set elsewhere in
+  #the finalise plot function
 
   #Legend format
-  #This sets the position and alignment of the legend, removes a title and backround for it and sets the requirements for any text within the legend. The legend may often need some more manual tweaking when it comes to its exact position based on the plot coordinates.
+  #This sets the position and alignment of the legend, removes a title and
+  #background for it and sets the requirements for any text within the legend.
+  #The legend may often need some more manual tweaking when it comes to its
+  #exact position based on the plot coordinates.
   legend.position = "top",
   legend.text.align = 0,
   legend.background = ggplot2::element_blank(),
@@ -34,7 +39,10 @@ tntp_style <- function() {
                              color="#222222"),
 
   #Axis format
-  #This sets the text font, size and colour for the axis test, as well as setting the margins and removes lines and ticks. In some cases, axis lines and axis ticks are things we would want to have in the chart - the cookbook shows examples of how to do so.
+  #This sets the text font, size and colour for the axis test, as well as setting
+  #the margins and removes lines and ticks. In some cases, axis lines and axis
+  #ticks are things we would want to have in the chart - the cookbook shows
+  #examples of how to do so.
   axis.title = ggplot2::element_blank(),
   axis.text = ggplot2::element_text(family=font,
                            size=18,
@@ -44,16 +52,22 @@ tntp_style <- function() {
   axis.line = ggplot2::element_blank(),
 
   #Grid lines
-  #This removes all minor gridlines and adds major y gridlines. In many cases you will want to change this to remove y gridlines and add x gridlines. The cookbook shows you examples for doing so
+  #This removes all minor gridlines and adds major y gridlines. In many cases
+  #you will want to change this to remove y gridlines and add x gridlines. The
+  #cookbook shows you examples for doing so
   panel.grid.minor = ggplot2::element_blank(),
   panel.grid.major.y = ggplot2::element_line(color="#cbcbcb"),
   panel.grid.major.x = ggplot2::element_blank(),
 
   #Blank background
-  #This sets the panel background as blank, removing the standard grey ggplot background colour from the plot
+  #This sets the panel background as blank, removing the standard grey ggplot
+  #background colour from the plot
   panel.background = ggplot2::element_blank(),
 
-  #Strip background (#This sets the panel background for facet-wrapped plots to white, removing the standard grey ggplot background colour and sets the title size of the facet-wrap title to font size 22)
+  #Strip background
+  #This sets the panel background for facet-wrapped plots to
+  #white, removing the standard grey ggplot background colour and sets the title
+  #size of the facet-wrap title to font size 22)
   strip.background = ggplot2::element_rect(fill="white"),
   strip.text = ggplot2::element_text(size  = 22,  hjust = 0)
   )
