c8289ca4c773eac3db13b05ca1c49258dfaadcc8
Sam Talcott <sam.talcott@tntp.org>
2024-01-08 12:24:54 -0500

    Fixed all testing warnings

--- tests/testthat/test-date-to-sy.R
+++ tests/testthat/test-date-to-sy.R
@@ -1,9 +1,5 @@
 # Tests for date-to-school year function
 
-library(dplyr)
-library(lubridate)
-context("date_to_sy")
-
 test_that("conversion is accurate", {
   expect_equal(date_to_sy(as.Date("2014-05-05"), as.Date("2000-07-01")), "2013 - 2014")
   expect_equal(date_to_sy(as.Date("2014-07-05"), as.Date("2000-07-01")), "2014 - 2015")
