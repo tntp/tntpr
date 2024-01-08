e1bd5ee06e8246e48ab57317d8613df68c829019
Sam Talcott <sam.talcott@tntp.org>
2024-01-08 11:58:32 -0500

    Added tntp_cred_set() and an initial test

--- R/tntp_cred.R
+++ R/tntp_cred.R
@@ -3,18 +3,22 @@
 #' @description
 #' A wrapper around the `keyring` package for secure credential management.
 #'
-#' `tntp_cred()` will attempt to get a key, and if no credential is found it
-#' will prompt you to add it (and then return it). You can also use this to
-#' just set (or overwrite) values by using the `.set` parameter.
+#' `tntp_cred()` will attempt to get a credential, and if no credential is found
+#' it will prompt you to add it (and then return it).
 #'
-#' `tntp_cred_list()` will list all current credentials by service and username
+#' `tntp_cred_set()` will set a credential. By default it will prompt before
+#' overwriting any current credentials.
+#'
+#' `tntp_cred_list()` will list all current credentials by sorted by service
+#' and username.
 #'
 #' @md
 #'
 #' @param service The identifier for the credential you are pulling or setting
 #' @param username OPTIONAL. Can be used to specify different usernames for the same service
 #' @param keyring OPTIONAL. Can be used to specify a specific keyring
-#' @param .set Logical. If `TRUE`, will prompt the user to set a credential and will not return a value. Essentially the same as `keyring::key_set()`.
+#' @param prompt OPTIONAL. What text should be displayed above the input box for the key while setting?
+#' @param overwrite OPTIONAL. By default, `tntp_cred_set()` will prompt if it finds a credential already saved. Set this to `TRUE` to overwrite without prompting or `FALSE` to throw an error if a current credential is found.
 #'
 #' @return A stored (or newly created) credential
 #' @export
@@ -33,44 +37,73 @@
 #' tntp_cred("QUALTRICS_TOKEN", .set = TRUE)
 #' }
 #'
-tntp_cred <- function(service, username = NULL, keyring = NULL, .set = FALSE) {
+tntp_cred <- function(service, username = NULL, keyring = NULL, prompt = NULL) {
 
-  # Check for and return a value for that key
-  if(!.set) {
-    # Pull with key_get, or return an error
+  # Pull with key_get, or return an error
+  cred <- tryCatch(keyring::key_get(service, username, keyring),
+                   error = function(e) e)
+
+  # If a credential isn't found, prompt user to create it
+  if(("error" %in% attr(cred, "class"))) {
+
+    cli::cli_inform(c("i" = "No credentials found for {.val {service}}",
+                      "i" = "To list all current credentials stop the script and run {.run tntpr::tntp_cred_list()}",
+                      "","Would you like to set new credentials for {.val {service}} now?"))
+
+    input <- utils::select.list(c("Yes","No"))
+
+    # If not, end with an error
+    if(input != "Yes") {
+      cli::cli_abort("No credentials found for {.val {service}}.")
+    }
+
+    # If yes, set and then re-pull the credential
+    tntp_cred_set(service, username, keyring, prompt, overwrite = TRUE)
+    cred <- keyring::key_get(service, username, keyring)
+
+  }
+
+  # Return the credential
+  cred
+
+}
+
+#' @export
+#' @rdname tntp_cred
+tntp_cred_set <- function(service = NULL, username = NULL, keyring = NULL, prompt = NULL, overwrite = NULL) {
+
+  if(is.null(prompt)) prompt <- paste0("Enter credential for '", service, "': ")
+
+  # If overwrite is TRUE, skip to writing
+  if(!isTRUE(overwrite)) {
+
+    # Check for existence of key by looking for an error in key_get()
     cred <- tryCatch(keyring::key_get(service, username, keyring),
                      error = function(e) e)
 
-    # If a credential is found, return it!
+    # If a credential is found
     if(!("error" %in% attr(cred, "class"))) {
-      return(cred)
 
-      # If no credential is found, ask if a user wants to create it
-    } else {
-      cli::cli_inform(c("i" = "No credentials found for {.val {service}}",
-                        "i" = "To list all current credentials stop the script and run {.run tntpr::tntp_cred_list()}",
-                        "","Would you like to set new credentials for {.val {service}} now?"))
+      # If overwrite == FALSE, end with error
+      if(isFALSE(overwrite)) cli::cli_abort(c("x" = "Credential already found for this service and username.",
+                                             "i" = "To overwrite, run with parameter {.code overwrite = TRUE}"))
 
-      input <- utils::select.list(c("Yes","No"))
-
-      # If not, end with an error
-      if(input != "Yes") {
-        cli::cli_abort("No credentials found for {.val {service}}.")
-      }
+      # If overwrite is anything else (NA), warn about duplicate and prompt to overwrite
+      cli::cli_inform(c("i" = "Credential already found for this service and username. Overwrite?"))
+      if(select.list(c('Overwrite with new credential','Cancel')) == 'Cancel') cli::cli_abort("Credentials not updated")
     }
   }
 
-  # If no value exists, ask if they would like to set a value, then set a value
-  keyring::key_set(service, username, keyring,
-                   prompt = paste0("Enter credential for '", service, "': "))
-
-  # Return the newly set value
-  if(!.set) keyring::key_get(service, username, keyring)
+  keyring::key_set(service, username, keyring, prompt)
 
 }
 
 #' @export
 #' @rdname tntp_cred
 tntp_cred_list <- function(service = NULL, keyring = NULL) {
-  keyring::key_list(service, keyring)
+  # Pull with key_list
+  list <- keyring::key_list(service, keyring)
+
+  # Sort by service, then username
+  list[order(list$service, list$username),]
 }
